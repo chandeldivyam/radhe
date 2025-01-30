c22ca951615efd696b9cca5512b7ac1fcd54e1f4
Divyam Chandel <chandeldivyam@gmail.com>
2025-01-30 23:20:09 +0530

    format and lint issues

--- frontend/app/(public)/login/page.tsx
+++ frontend/app/(public)/login/page.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
-import { useState } from 'react';
-
 export default function Login() {
-	return <>Login Page</>
+	return <>Login Page</>;
 }
